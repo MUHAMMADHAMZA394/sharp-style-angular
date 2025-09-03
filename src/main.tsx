14cb3be2256432aeb08c908d28cf3fd043622282
Muhammad Hamza <87033543+MUHAMMADHAMZA394@users.noreply.github.com>
2025-09-03 16:38:17 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename="/sharp-style-angular">
     <App />
   </BrowserRouter>
